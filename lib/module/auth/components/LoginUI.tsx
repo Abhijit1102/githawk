08a17a5872702cba573423285b86e75d05a1eaf1
Abhijit1102 <abhijitrajkumar2@gmail.com>
2026-01-15 12:24:48 +0530

    minor fixed 06

--- lib/module/auth/components/LoginUI.tsx
+++ lib/module/auth/components/LoginUI.tsx
@@ -10,7 +10,7 @@ const LoginUI = () => {
   const handleGithubLogin = async () => {
     setIsLoading(true);
     try {
-      await signIn.social({ provider: "github", callbackURL: "/dashboard" });
+      await signIn.social({ provider: "github", callbackURL: "/" });
     } catch (err) {
       console.error(err);
       setIsLoading(false);
