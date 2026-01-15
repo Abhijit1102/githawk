b5922559e8a4887eec7040d20c8e5bb427325ebf
Abhijit1102 <abhijitrajkumar2@gmail.com>
2026-01-15 11:40:02 +0530

    fixed 03

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,13 @@
 import { createAuthClient } from "better-auth/react";
 import { polarClient } from "@polar-sh/better-auth";
+
 export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL,
+    baseURL:process.env.BETTER_AUTH_URL
+})
+
+export const { signIn, signUp, useSession, signOut, customer, checkout} = createAuthClient({
+    basePath: process.env.BETTER_AUTH_URL,
     plugins:[polarClient()]
 })
-export const { signIn, signUp, useSession, signOut, customer, checkout } = authClient;
+
+
