4c07c6c1074929e8ecb36f1acb97b46bfd0b09bd
Abhijit1102 <abhijitrajkumar2@gmail.com>
2026-01-15 18:57:14 +0530

    inggest fixed

--- inngest/client.ts
+++ inngest/client.ts
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 
-// Create a client to send and receive events
-export const inngest = new Inngest({ id: "githawk" });
-
+export const inngest = new Inngest({
+  id: "githawk",
+  eventKey: process.env.INNGEST_EVENT_KEY!, 
+});
